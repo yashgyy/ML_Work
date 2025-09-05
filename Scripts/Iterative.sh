717db331f3a2ae1b4f953c54ac679a5dc512dac4
Yash <yashgyy@gmail.com>
2025-09-05 13:06:24 +0530

    Refactored for AMD Machines

--- Scripts/Iterative.sh
+++ Scripts/Iterative.sh
@@ -41,14 +41,14 @@ echo ""
 
 # Define applications to profile
 declare -A applications=(
-    ["Naive_Bayes"]="../Naive_Bayes/server ../Naive_Bayes/Server/performance.csv"
-    ["Logistic_Regression"]="../Logistic_Regression/server ../Logistic_Regression/Server/performance.csv"
-    ["Linear_Regression"]="../Linear_Regression/server ../Linear_Regression/Server/performance.csv"
-    ["KernelSVM"]="../KernelSVM/server ../KernelSVM/Server/performance.csv"
-    ["LSVM"]="../LSVM/server ../LSVM/Server/performance.csv"
-    ["KMeans"]="../KMeans/server ../KMeans/Server/performance.csv"
-    ["Adaboost"]="../Adaboost/server ../Adaboost/Server/performance.csv"
-    ["RF"]="../RF/server ../RF/Server/performance.csv"
+    ["Naive_Bayes"]="../Naive_Bayes/server ../Naive_Bayes/AMDS_AMDC_micro_performance.csv"
+    ["Logistic_Regression"]="../Logistic_Regression/server ../Logistic_Regression/AMDS_AMDC_micro_performance.csv"
+    ["Linear_Regression"]="../Linear_Regression/server ../Linear_Regression/AMDS_AMDC_micro_performance.csv"
+    ["KernelSVM"]="../KernelSVM/server ../KernelSVM/AMDS_AMDC_micro_performance.csv"
+    ["LSVM"]="../LSVM/server ../LSVM/AMDS_AMDC_micro_performance.csv"
+    ["KMeans"]="../KMeans/server ../KMeans/AMDS_AMDC_micro_performance.csv"
+    ["Adaboost"]="../Adaboost/server ../Adaboost/AMDS_AMDC_micro_performance.csv"
+    ["RF"]="../RF/server ../RF/AMDS_AMDC_micro_performance.csv"
 )
 
 # Process each application
