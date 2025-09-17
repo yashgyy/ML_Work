8c5e988ec80b3f5f30eb30732131413203604e56
Yash <yashgyy@gmail.com>
2025-09-18 00:59:51 +0530

    Segmented

--- Scripts/Iterative.sh
+++ Scripts/Iterative.sh
@@ -27,7 +27,7 @@ run_profiling() {
     
     # Start server with profiling
     echo "Launching server with profiler..."
-    $AMD1 -m ipc,fp,l1,l2,memory,l3 -d 300 -o "$output_path" -- "$app_path"
+    $AMD1 -m ipc,fp,l1,l2,memory,l3 -d 30 -o "$output_path" -- "$app_path"
     
     echo "Server profiling completed for $app_name"
     echo "Output saved to: $output_path"
